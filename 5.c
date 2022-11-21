21b33c3588a0d83659145f8f2ff7bf0db2252a46
Vaibhav Tiwari <107205805+Vaibhav882@users.noreply.github.com>
2022-11-21 23:37:50 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+       int a[10],i,count=0;
+       printf("ENTER 10 NUMBER\n");
+       for(i=0;i<=9;i++)
+              scanf("%d",&a[i]);
+       for(i=0;i<=8;i++)
+       {
+              if(a[i]==a[i+1])
+                     count++;
+
+       if(count==1)
+              break;
+      }
+}
